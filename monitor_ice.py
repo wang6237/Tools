eadf8df7794e1b656c856a518332a0592ee306f8
wang6237 <ykwang@vip.qq.com>
2024-09-04 15:52:24 +0800

    Update monitor_ice.py
    使用deepseek优化

--- monitor_ice.py
+++ monitor_ice.py
@@ -2,94 +2,74 @@
 # -*- coding:utf-8 -*-
 __author__ = 'wangyk'
 
-import commands,time
+import subprocess
+import time
 import threading
-from multiprocessing import Process
 import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
+import configparser
 
 # 可以屏蔽一些已关闭的工程
 bad_list = []
 
-class conn_ice_grid(object):
-    def __init__(self,ice_grid_server,ice_grid_port,default_Locator_name):
+class ConnIceGrid(object):
+    def __init__(self, ice_grid_server, ice_grid_port, default_locator_name):
         self.user = 'admin'
-        self.password = 'xxxx'  #密码
+        self.password = 'xxxx'  # 密码
         self.grid_server = ice_grid_server
         self.grid_port = ice_grid_port
-        self.default_Locator = default_Locator_name
+        self.default_locator = default_locator_name
         self.run()
 
     def get_server_list(self):
         # 获取在线server的列表
-        print "start get server list"
-        server_list_cmd = "icegridadmin --Ice.Default.EncodingVersion=1.0 --Ice.Default.Locator='%sIceGrid/Locator:tcp -h %s -p %s' -u %s -p '%s' -e 'server  list'" % (self.default_Locator,self.grid_server,self.grid_port,self.user,self.password)
-        server_list = commands.getstatusoutput(server_list_cmd)[1].split('\n')
-        if len(server_list) == 0 :
-            self.sendmail("获取ICE Server 失败！！，请见检查服务器")
-        self.s_list = []
-        for k in server_list:
-            if "icegridadmin" in k:
-                pass
-            else:
-                # 判断是否在bad_list中
-                if k in bad_list:
-                    pass
-                else:
-                    self.s_list.append(k)
+        print("start get server list")
+        server_list_cmd = f"icegridadmin --Ice.Default.EncodingVersion=1.0 --Ice.Default.Locator='{self.default_locator}IceGrid/Locator:tcp -h {self.grid_server} -p {self.grid_port}' -u {self.user} -p '{self.password}' -e 'server list'"
+        server_list = subprocess.getoutput(server_list_cmd).split('\n')
+        if not server_list:
+            self.sendmail("获取ICE Server 失败！！，请检查服务器")
+        self.s_list = [k for k in server_list if "icegridadmin" not in k and k not in bad_list]
+
     def monitor(self):
-        print "check server"
- #       print self.s_list
-
+        print("check server")
         for service_name in self.s_list:
-            server_state_cmd = "icegridadmin --Ice.Default.EncodingVersion=1.0 --Ice.Default.Locator='%sIceGrid/Locator:tcp -h %s -p %s'  -u %s -p '%s' -e 'server  state %s'" % (self.default_Locator,self.grid_server,self.grid_port,self.user,self.password,service_name)
-            for k1 in commands.getstatusoutput(server_state_cmd)[1].split('\n'):
-                if  k1.startswith("error:"):
-                    #print service_name
-                    msg = "%s %s is Warning on %s %s"  % (service_name,k1,self.grid_server,self.default_Locator)
-
-                    if service_name in bad_list:
-                        print "\033[41;1m %s\033[0m" % msg
-                    else:
-                        print "start mail...."
+            server_state_cmd = f"icegridadmin --Ice.Default.EncodingVersion=1.0 --Ice.Default.Locator='{self.default_locator}IceGrid/Locator:tcp -h {self.grid_server} -p {self.grid_port}' -u {self.user} -p '{self.password}' -e 'server state {service_name}'"
+            state_output = subprocess.getoutput(server_state_cmd).split('\n')
+            for k1 in state_output:
+                if k1.startswith("error:"):
+                    msg = f"{service_name} {k1} is Warning on {self.grid_server} {self.default_locator}"
+                    if service_name not in bad_list:
+                        print(f"\033[41;1m {msg}\033[0m")
                         self.sendmail(msg)
+                elif k1.startswith("active") and "enabled" in k1:
+                    print(f"{time.ctime()}: \033[32;1m {service_name} {k1} is OK\033[0m")
 
-                elif  k1.startswith("active") and "enabled" in k1:
-                    print "%s: \033[32;1m %s %s  is OK\033[0m" % (time.ctime(),service_name,k1)
-                else:
-                    pass
-
-    def sendmail(self,*args):  #邮箱需要重新定义
-        msg = MIMEText(args[0], 'plain', 'utf-8')
-        msg['From'] = formataddr(["ICE 监控告警",'alarm1@test.com'])
-        msg['To'] = formataddr(["用户","运维监控中心"])
+    def sendmail(self, message):
+        msg = MIMEText(message, 'plain', 'utf-8')
+        msg['From'] = formataddr(["ICE 监控告警", 'alarm1@test.com'])
+        msg['To'] = formataddr(["用户", "运维监控中心"])
         msg['Subject'] = "ICE 监控告警"
 
         server = smtplib.SMTP("smtp.sina.com", 25)
         server.login("alarm1@sina.com", "usr/index_usr.jsp")
-        server.sendmail('alarm1@sina.com', ['yunwei@sina.com',], msg.as_string())
+        server.sendmail('alarm1@sina.com', ['yunwei@sina.com'], msg.as_string())
         server.quit()
 
     def run(self):
-        print "Start...."
-  #      print self.grid_server,self.grid_port,self.default_Locator
+        print("Start....")
         while True:
             self.get_server_list()
             self.monitor()
-            print "waiting"
+            print("waiting")
             time.sleep(60)
 
-
-
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-    #使用线程
-    t1 = threading.Thread(target=conn_ice_grid,args=("172.16.30.x","4061","Account"))
+    t1 = threading.Thread(target=ConnIceGrid, args=(config['IceGrid']['Server1'], config['IceGrid']['Port1'], config['IceGrid']['Locator1']))
     t1.start()
 
-    t3 = threading.Thread(target=conn_ice_grid,args=("172.16.30.x","4061","Business"))
+    t3 = threading.Thread(target=ConnIceGrid, args=(config['IceGrid']['Server2'], config['IceGrid']['Port2'], config['IceGrid']['Locator2']))
     t3.start()
-
-
-
